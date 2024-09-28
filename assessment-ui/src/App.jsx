7b405d77b7c77c1901c0121abef49690bfbaad20
Leticia Saraiva <leticia.saraiva013@gmail.com>
2024-09-28 19:38:30 -0300

    feat: ajusting assets names and creating menu

--- assessment-ui/src/App.jsx
+++ assessment-ui/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
+import Header from './Header'
 
 function App() {
 
 
   return (
     <>
-
+  <Header/>
     </>
   )
 }
--- assessment-ui/src/Header.jsx
+++ assessment-ui/src/Header.jsx
@@ -1,3 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import logo from './assets/Logotipo-Lara-Transp-Azul.svg'
+
 const Header = () => {
-    
+    return(
+        <header className="header-container">
+            <img className='header-logoSvg' src={logo} alt="Lara Saraiva" />
+            <button><FontAwesomeIcon icon={faBars} /></button>
+
+            <nav className='header-menu'>
+                <ul>
+                    <li>Biografia</li>
+                    <li>Propostas</li>
+                    <li>Agenda</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
+
+export default Header
