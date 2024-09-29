c425b8114c31bab73be1d076382295dd452c2bb6
Leticia Saraiva <leticia.saraiva013@gmail.com>
2024-09-29 12:07:29 -0300

    style: alter font

--- assessment-ui/src/Biography.jsx
+++ assessment-ui/src/Biography.jsx
@@ -1,7 +1,9 @@
 const Biography = () => {
     return (
-        <section>
-            <h1>Biografia</h1>
+        <section id="Biografia">
+            <div className="biography-title">
+                <h1 className="title">Biografia</h1>
+            </div>
            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit reiciendis blanditiis ipsa. Placeat, quos perspiciatis? Minus, dolorum? Numquam possimus laboriosam, voluptatem ullam minus neque sunt laborum porro, recusandae suscipit unde.</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, repellendus minima labore possimus magni corrupti et, velit qui accusamus perspiciatis placeat obcaecati nam aut laudantium nostrum suscipit praesentium vel adipisci!</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem beatae saepe quae vero repellendus iure optio ad rem! Ipsa animi voluptate sunt? Quibusdam natus nisi eligendi perspiciatis, facere quis facilis?</p>
