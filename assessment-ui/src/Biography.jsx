023a9b8d5510018ccc4a9b5890106921c2d9aeb8
Leticia Saraiva <leticia.saraiva013@gmail.com>
2024-09-29 14:59:28 -0300

    style: Biography component style

--- assessment-ui/src/Biography.jsx
+++ assessment-ui/src/Biography.jsx
@@ -5,10 +5,10 @@ import title from './assets/Biografia.svg'
 const Biography = () => {
     return (
         <section id="Biografia">
-            <h1 className="biography-title">
+            <div className="biography-title">
                 <div><img className="bio-title-h1" src={title} alt="Biografia" /></div>
                 <div className="bio-title-img"><img className="title-img" src={Lara} alt="Lara Saraiva"/></div>
-            </h1>
+            </div>
            <div className="biography-content">
             <p className="bio-text-p">Olá, sou Lara, uma spitz de 5 anos e candidata a prefeita, com uma visão clara para transformar nossa cidade em um lugar mais justo e eficiente. Uma das minhas principais propostas é investir R$ 1,8 bilhões na reforma e reorganização dos Centros Integrados de Educação Pública (CIEPs), garantindo acesso a educação de qualidade, além de promover esportes e lazer para todos.</p>
             <p className="bio-text-p">Para melhorar o sistema de saúde, proponho a criação de um aplicativo que permitirá o acesso direto ao Sistema Nacional de Regulação, facilitando o acompanhamento das filas. Também estou comprometida com políticas de habitação social, realocando moradores que vivem em áreas de risco de deslizamentos, como mostram os estudos do município.</p>
